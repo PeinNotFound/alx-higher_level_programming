f685b37b7cf2819957e62cb33631d3f1330f1205
PeinNotFound <akatsuki_ahmed@yahoo.com>
2023-11-02 12:45:19 -0700

    argu 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,18 @@
 
 import sys
 
-def main():
-    args = sys.argv[1:]
-    count = len(args)
-    print(f"{count} argument{'s' if count != 1 else ''}:")
-    for i, arg in enumerate(args, start=1):
-        print(f"{i}: {arg}")
+def print_arguments():
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
+        print("No arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    for index, arg in enumerate(sys.argv[1:], 1):
+        print(f"{index}: {arg}")
 
 if __name__ == "__main__":
-    main()
+    print_arguments()
