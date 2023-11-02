479031fbcde89844f844b31f0156f132a8a477a4
PeinNotFound <akatsuki_ahmed@yahoo.com>
2023-11-02 13:07:08 -0700

    hidden pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
 
-import dis
-import types
-import importlib.util
-
-def list_module_names(module_file):
-    spec = importlib.util.spec_from_file_location("hidden_4", module_file)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    names = dir(module)
-    names.sort()
-
-    for name in names:
-        if not name.startswith("__"):
-            print(name)
-
 if __name__ == "__main__":
-    list_module_names('/home/pexn/Downloads')
+    import hidden_4
+    for test in dir(hidden_4):
+        if test[0] != '_':
+            print(test)
