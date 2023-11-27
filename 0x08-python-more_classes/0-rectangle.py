62752923b06f37edb14a5669cf412faa8f2fdfd8
PeinNotFound <akatsuki_ahmed@yahoo.com>
2023-11-27 09:00:26 -0800

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-'''
-this is an empty rectangle class
-'''
+
+'''this is an empty rectangle class'''
+
 class Rectangle:
     '''
     this is rectangle class
     '''
     pass
+
