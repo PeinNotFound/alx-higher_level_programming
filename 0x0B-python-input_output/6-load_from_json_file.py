f20e2e1dee25e67a4083aaf0812ec816772ac4d7
PeinNotFound <akatsuki_ahmed@yahoo.com>
2023-12-06 11:37:10 -0800

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""code defines a function for loading a
+Python object from a JSON file."""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Create a Python object from a JSON file
+    """
+    with open(filename) as f:
+        return json.load(f)
