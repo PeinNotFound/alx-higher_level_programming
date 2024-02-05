5eab13a2f62c9ecf62a0e70d5bbaf03142ef8132
PeinNotFound <akatsuki_ahmed@yahoo.com>
2024-02-05 15:22:04 -0800

    tests

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This code defines a custom list class called MyList,
+whiches inherits from the built-in list class.
+It includes a method for printing the list in sorted order."""
+
+class MyList(list):
+    """Provides sorted printing for the built-in list class."""
+
+    def print_sorted(self):
+        """Prints the list in ascending sorted order."""
+        print(sorted(self))
