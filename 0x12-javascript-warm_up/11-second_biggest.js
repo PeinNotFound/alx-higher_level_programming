084456da65ef71f1d4c758c333ff888238575917
PeinNotFound <akatsuki_ahmed@yahoo.com>
2024-04-08 22:52:57 +0000

    making progress

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  let first = Number(argv[2]);
+  let second = Number(argv[3]);
+
+  for (let i = 3; i < argv.length; i++) {
+    const currentNumber = Number(argv[i]);
+
+    if (currentNumber > first) {
+      second = first;
+      first = currentNumber;
+    } else if (currentNumber > second && currentNumber < first) {
+      second = currentNumber;
+    }
+  }
+
+  console.log(second);
+}
